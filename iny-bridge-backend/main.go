903c8117ab8df2a4491365b55f771c2ec791a2da
gevorgyan.la <gevorgyan.la@elt.cor>
2026-03-10 00:53:23 +0700

    feat: added room and client struct

--- iny-bridge-backend/main.go
+++ iny-bridge-backend/main.go
@@ -9,7 +9,16 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func server() {
+type Client struct {
+	conn   *websocket.Conn
+	roomId string
+}
+
+type Hub struct {
+	rooms map[string][]*Client
+}
+
+func server(hub *Hub) {
 	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
@@ -19,38 +28,59 @@ func server() {
 		return c.String(http.StatusOK, "Hello, HTTP!")
 	})
 
-	e.GET("/ws", wsHandler)
+	e.GET("/ws", wsHandler(hub))
 
 	if err := e.Start(":1323"); err != nil {
 		e.Logger.Fatal("failed to start server", err)
 	}
 }
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
-func wsHandler(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer ws.Close()
-
-	for {
-		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+func wsHandler(hub *Hub) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 		if err != nil {
-			c.Logger().Error("failed to write WS message", "error", err)
+			return err
+		}
+		client := &Client{
+			conn:   ws,
+			roomId: "1",
+		}
+		
+		hub.rooms[client.roomId] = append(hub.rooms[client.roomId], client)
+		
+		defer ws.Close()
+
+		for {
+			// Write
+			err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+			if err != nil {
+				c.Logger().Error("failed to write WS message", "error", err)
+				break
+			}
+
+			// Read
+			_, msg, err := ws.ReadMessage()
+			if err != nil {
+				c.Logger().Error("failed to read WS message", "error", err)
+				break
+			}
+			fmt.Printf("%s\n", msg)
 		}
 
-		// Read
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			c.Logger().Error("failed to read WS message", "error", err)
-		}
-		fmt.Printf("%s\n", msg)
+		return nil
 	}
+
 }
 
 func main() {
-	server()
+	hub := &Hub{
+		rooms: make(map[string][]*Client),
+	}
+	server(hub)
 }
